fa02bbdfcf75dd1f44b7bc4bd247b5743d9c3c38
shekharsolanki0000 <sheksol0000@gmail.com>
2022-07-10 10:44:39 +0530

    Second Commit

--- src/main/java/com/project/Employee/error/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/project/Employee/error/RestResponseEntityExceptionHandler.java
@@ -1,19 +1,24 @@
 package com.project.Employee.error;
 
 import com.project.Employee.entity.ErrorMessage;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 @ResponseStatus
+@Slf4j
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
-
+    @ExceptionHandler(EmployeeNotFoundException.class)
     public ResponseEntity<ErrorMessage> employeeNotFoundException(EmployeeNotFoundException exception,
                                                                   WebRequest request) {
+        log.info("Inside fetchEmployeeById method of EmployeeServiceImpl");
 
         ErrorMessage message = new ErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage());
 
